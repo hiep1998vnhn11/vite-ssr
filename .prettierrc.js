07862811b9510f89c484ede39c938a033bbbd414
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2021-10-13 15:50:04 +0700

    update

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex">
+    <div>123</div>
+    <div>321</div>
+  </div>
+</template>
+<script lang="ts">
+import { onMounted, ref, defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    onMounted(() => {})
+    return {}
+  },
+})
+</script>
+<style lang="scss" scoped></style>
